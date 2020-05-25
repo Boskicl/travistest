577377a128f66f6eb81ba3e1a2ec0029012e4c1f
Ljube <61674765+Boskicl@users.noreply.github.com>
2020-05-25 10:46:32 -0700

    new calc2

--- tests/testc.py
+++ tests/testc.py
@@ -1,6 +1,5 @@
 from Calc import Calc
 
-def test_add():
-    x,y = 1,2
-    instance = Calc(x,y)
-    assert instance.add() == x + y, "Add method"
+x,y = 1,2
+instance = Calc(x,y)
+assert instance.add() == x + y, "Add method"
