2f85d6a426e8181f2785c556a9503a65efd812fa
Ljube_Windows <Ljuboslavboskic@gmail.com>
2020-05-25 10:55:35 -0700

    new should work

--- testc.py
+++ testc.py
@@ -3,4 +3,7 @@ from Calc import Calc
 def test_add():
     x,y = 1,2
     instance = Calc(x,y)
-    assert instance.add() == x + y, "Add method"
+    if instance.add() == x+y:
+        print('True')
+
+test_add()
